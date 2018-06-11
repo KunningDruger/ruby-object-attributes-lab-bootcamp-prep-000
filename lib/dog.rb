501d5ad2c62d50da4580799eead67731ad4ca37c
Ciaran Morinan <cmorinan@gmail.com>
2018-06-11 20:26:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
   
-  def breed=(breed)
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+end
